7f93c6e7d23b127fc491b3245fffeb4ab23d8536
gabrielPelegri <gabriel.pelegri.riis@gmail.com>
2022-03-10 19:00:20 +0100

    Update Selector.java

--- Entrega7/src/Selector.java
+++ Entrega7/src/Selector.java
@@ -6,7 +6,8 @@ int eleccion=0;
         while(true){
             System.out.println("1: Ejercicio 1 (Dados)");
             System.out.println("2: Ejercicio 2 (Clientes)");
-            System.out.println("2: Ejercicio 2 (Barcos II)");
+            System.out.println("3: Ejercicio 3 (Barcos III)");
+            System.out.println("4: Ejercicio 4 (Barcos IV)");
             
     try{
         eleccion=teclado.nextInt();
